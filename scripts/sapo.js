e344035994844025e35e6be4a3090b8634e4d337
Đạt Trần <dattran@ats-MacBook-Pro.local>
2024-01-10 15:12:32 +0700

    update frame camera

--- scripts/sapo.js
+++ scripts/sapo.js
@@ -44,46 +44,37 @@ const cameras = {
     main: "#3079FF",
     primary: "#EAF1FF",
   },
-  _progressBar: null,
+  frames: {
+    width: 470,
+    height: 660,
+    option: 0,
+  },
+  _videoLive: null,
   _faceRecord: null,
   _confirm: null,
   _retake: null,
   _faceAuthenForm: null,
-  _videoLive: null,
   _mediaRecorded: null,
   _message: null,
-  _timer: null,
-  _canvas: null,
-  _loader: null,
   _snap: null,
-  timer: null,
-  faceVerify: false,
   isMediaRecorderSupported: false,
   preTrainModel: null,
   device: null,
-  ctx: null,
   stream: null,
   mediaRecorder: null,
-  faceRunsInterval: null,
-  progressInterval: null,
   standardDeviation: { x: 0, y: 0 },
   init: async function () {
     LoadingAnimation.start();
     LoadingAnimation.display();
     this._faceAuthenForm = document.querySelector("#FaceAuthenForm");
-    this._faceRecord = document.querySelector("#faceRecord");
     this._videoLive = document.querySelector("#videoLive");
     this._confirm = document.querySelector("#confirm");
     this._retake = document.querySelector("#retake");
-    this._mediaRecorded = document.querySelector("#videoRecorded");
-    this._timer = document.querySelector("#timer");
     this._message = document.querySelector("#message");
+    this._faceRecord = document.querySelector("#faceRecord");
     this._snap = document.querySelector("#snap");
-    this._canvas = document.querySelector("#canvas");
-    this._progressBar = document.querySelector("#progressBar");
 
     this.device = cameras.getMobileOperatingSystem();
-    this.ctx = this._canvas.getContext("2d");
     this._mediaRecorded = document.querySelector("#imageRecorded");
 
     try {
@@ -103,12 +94,11 @@ const cameras = {
     try {
       // LoadingAnimation.display();
       const livecamera = document.querySelector("#my_camera");
-      this._message.textContent = "Chụp ảnh chính diện khuôn mặt";
-      this._timer.textContent = "";
+      // this._message.textContent = "Chụp ảnh chính diện khuôn mặt";
 
       Webcam.set({
-        width: this._progressBar.width,
-        height: this._progressBar.height,
+        width: this.frames.width,
+        height: this.frames.height,
         dest_width: null,
         dest_height: null,
         image_format: "jpeg",
@@ -132,12 +122,12 @@ const cameras = {
       Webcam.on("load", function () {
         const livecam = document.querySelectorAll("#my_camera video");
         livecam[0].style.transform = "none";
-        livecam[0].width = cameras._videoLive.width;
-        livecam[0].height = cameras._videoLive.height;
+        livecam[0].width = cameras.frames.width;
+        livecam[0].height = cameras.frames.height;
 
         cameras._videoLive = livecam[0];
-        cameras._canvas.style.transform = "none";
-        cameras._videoLive.classList.add("video");
+        // cameras._canvas.style.transform = "none";
+        cameras._videoLive.classList.add("videostream");
       });
 
       Webcam.on("live", function () {
@@ -169,8 +159,8 @@ const cameras = {
         cameras._mediaRecorded.style = "display:block";
 
         Webcam.set({
-          dest_width: cameras._videoLive.width,
-          dest_height: cameras._videoLive.height + 360,
+          dest_width: cameras.frames.width,
+          dest_height: cameras.frames.height, //+ 360
         });
 
         Webcam.freeze();
@@ -281,7 +271,7 @@ const cameras = {
         }
       }
     } catch (error) {
-      cameras.reset();
+      // cameras.reset();
       console.log("error while draw prediction: ", error);
       cameras._message.textContent = error.toString();
     }
@@ -403,5 +393,255 @@ const cameras = {
     }
 
     return "unknown";
+  },
+  reset: function (message = "") {
+    cameras._message.textContent = message;
+    cameras.faceVerify = false;
+
+    this._confirm.style = "display:none";
+  },
+};
+
+const frame = {
+  themes: {
+    main: "#3079FF",
+    primary: "#EAF1FF",
+  },
+  frame: {
+    width: "650px",
+    height: "480px",
+  },
+  _confirm: null,
+  _retake: null,
+  _faceAuthenForm: null,
+  _mediaRecorded: null,
+  _message: null,
+  _canvas: null,
+  _loader: null,
+  _snap: null,
+  faceVerify: false,
+  isMediaRecorderSupported: false,
+  preTrainModel: null,
+  device: null,
+  ctx: null,
+  stream: null,
+  mediaRecorder: null,
+  faceRunsInterval: null,
+  progressInterval: null,
+  standardDeviation: { x: 0, y: 0 },
+  init: async function (option) {
+    LoadingAnimation.start();
+    LoadingAnimation.display();
+    this._faceAuthenForm = document.querySelector("#FaceAuthenForm");
+    this._videoLive = document.querySelector("#videoLive");
+    this._confirm = document.querySelector("#confirm");
+    this._retake = document.querySelector("#retake");
+    this._message = document.querySelector("#message");
+    this._snap = document.querySelector("#snap");
+
+    this.device = cameras.getMobileOperatingSystem();
+    this.ctx = this._canvas.getContext("2d");
+    this._mediaRecorded = document.querySelector("#imageRecorded");
+
+    try {
+      this.preTrainModel = await blazeface.load();
+    } catch (error) {
+      console.log("init faceDetection failure: ", error);
+    }
+
+    // cameras.standardDeviation = { x: 175, y: -40 };
+
+    await cameras.startAndroidStream();
+    cameras.handleAndroidEvent();
+
+    LoadingAnimation.dispose();
+  },
+  startAndroidStream: async function () {
+    try {
+      // LoadingAnimation.display();
+      const livecamera = document.querySelector("#my_camera");
+      // this._message.textContent = "Chụp ảnh chính diện khuôn mặt";
+
+      Webcam.set({
+        width: cameras.frames.width,
+        height: cameras.frames.height,
+        dest_width: null,
+        dest_height: null,
+        image_format: "jpeg",
+        jpeg_quality: 90,
+        force_flash: false,
+        flip_horiz: true,
+        fps: 45,
+      });
+
+      Webcam.attach(livecamera);
+      LoadingAnimation.dispose();
+    } catch (error) {
+      console.log("init camera stream failure: ", error);
+      this._message.textContent = "Trình duyệt không hỗ trợ camera: " + error.toString();
+      LoadingAnimation.dispose();
+    }
+  },
+  handleAndroidEvent: function () {
+    try {
+      LoadingAnimation.display();
+      Webcam.on("load", function () {
+        const livecam = document.querySelectorAll("#my_camera video");
+        livecam[0].style.transform = "none";
+        livecam[0].width = cameras._videoLive.width;
+        livecam[0].height = cameras._videoLive.height;
+
+        cameras._videoLive = livecam[0];
+        // cameras._canvas.style.transform = "none";
+        // cameras._videoLive.classList.add("videostream");
+      });
+
+      Webcam.on("live", function () {
+        cameras._snap.style = "display:block";
+        cameras._retake.style = "display:none";
+        cameras._confirm.style = "display:none";
+        cameras._mediaRecorded.style = "display:none";
+      });
+
+      Webcam.on("error", function (err) {
+        console.log("error: ", err);
+      });
+
+      this._retake.addEventListener("click", function () {
+        LoadingAnimation.display();
+        cameras.startAndroidStream();
+        Webcam.unfreeze();
+      });
+
+      this._confirm.addEventListener("click", function () {
+        LoadingAnimation.display();
+      });
+
+      this._snap.addEventListener("click", function () {
+        LoadingAnimation.display();
+        cameras._message.innerHTML = `<strong style="color:${cameras.themes.main}">Loading...</strong>`;
+        cameras._snap.style = "display:none";
+        cameras._retake.style = "display:block";
+        cameras._mediaRecorded.style = "display:block";
+
+        Webcam.set({
+          dest_width: cameras._videoLive.width,
+          dest_height: cameras._videoLive.height, //+ 360
+        });
+
+        Webcam.freeze();
+        Webcam.snap(async function (data_uri, frame, context) {
+          cameras._mediaRecorded.src = data_uri;
+
+          Webcam.reset();
+
+          const prediction = await cameras.preTrainModel.estimateFaces(frame, false);
+          cameras.processFrameResults(cameras.ctx, frame, prediction);
+          LoadingAnimation.dispose();
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      LoadingAnimation.dispose();
+      this._message.textContent = error.toString();
+    }
+  },
+  detectFaces: async function () {
+    try {
+      // const estimationConfig = { flipHorizontal: true };
+      const prediction = await cameras.preTrainModel.estimateFaces(cameras._videoLive, false);
+
+      cameras.processLivenessResults(cameras.ctx, cameras._videoLive, prediction);
+    } catch (error) {
+      console.log("preTrainModel not found: ", error);
+    }
+  },
+  processFrameResults: function (ctx, frame, prediction) {
+    try {
+      if (prediction == undefined || prediction.length == 0) {
+        this.reset("Không tìm thấy khuôn mặt trong khung hình vui lòng chụp lại ảnh");
+      } else if (prediction.length > 1) {
+        this.reset("Chỉ cho phép 1 khuôn mặt trong khung hình vui lòng chụp lại ảnh");
+      } else {
+        const probability = prediction[0].probability[0];
+
+        if (prediction[0].topLeft[0] < 30) {
+          this.reset("Giữ cho khuôn mặt ở chính giữa và cách màn hình khoảng 25cm");
+          //left
+        } else if (prediction[0].topLeft[0] > 290) {
+          this.reset("Giữ cho khuôn mặt ở chính giữa và cách màn hình khoảng 25cm");
+          //right
+        } else if (prediction[0].topLeft[1] < 100) {
+          // top
+          this.reset("Giữ cho khuôn mặt ở chính giữa và cách màn hình khoảng 25cm");
+        } else if (prediction[0].topLeft[1] > 360) {
+          this.reset("Giữ cho khuôn mặt ở chính giữa và cách màn hình khoảng 25cm");
+          //bottom
+          // this.reset("Giữ cho khuôn mặt ở chính giữa và cách màn hình khoảng 25cm");
+        } else if (probability < 0.999) {
+          this.reset("Giữ cho khuôn mặt không bị che chắn và cách màn hình khoảng 25cm");
+        } else {
+          this._message.innerHTML = "Thực hiện thành công";
+          cameras._confirm.style = "display:block";
+
+          const loadFileToDocument = async function () {
+            const response = await fetch(cameras._mediaRecorded.src);
+            const blob = await response.blob();
+            const fileName = cameras.generateUUID();
+
+            const file = new File([blob], `${fileName}.jpg`, {
+              type: "image/jpeg",
+              lastModified: new Date(),
+            });
+
+            const dataTransfer = new DataTransfer();
+            dataTransfer.items.add(file);
+            cameras._faceRecord.files = dataTransfer.files;
+          };
+
+          loadFileToDocument();
+        }
+      }
+    } catch (error) {
+      // cameras.reset();
+      console.log("error while draw prediction: ", error);
+      cameras._message.textContent = error.toString();
+    }
+  },
+  generateUUID: function () {
+    var d = new Date().getTime();
+    var d2 = (typeof performance !== "undefined" && performance.now && performance.now() * 1000) || 0;
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16;
+      if (d > 0) {
+        r = (d + r) % 16 | 0;
+        d = Math.floor(d / 16);
+      } else {
+        r = (d2 + r) % 16 | 0;
+        d2 = Math.floor(d2 / 16);
+      }
+      return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+    });
+  },
+  getMobileOperatingSystem: function () {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
+    if (/android/i.test(userAgent)) {
+      return "ANDROID";
+    }
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "IOS";
+    }
+
+    return "unknown";
+  },
+  reset: function (message = "") {
+    cameras._message.textContent = message;
+    cameras.faceVerify = false;
+
+    this._confirm.style = "display:none";
   },
 };
